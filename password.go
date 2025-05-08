c90b8f40efeb51111158a78b7dd922da0914e28a
xbanki <contact@xbanki.me>
2025-05-08 16:38:18 +0300

    Add ability to create password from raw JSON

--- password.go
+++ password.go
@@ -1,5 +1,7 @@
 package main
 
+import "github.com/buger/jsonparser"
+
 func CreatePassword(username string, password string, domain string) *Password {
 	r := new(Password)
 	r.Policies = make([]Policy, 0)
@@ -7,6 +9,22 @@ func CreatePassword(username string, password string, domain string) *Password {
 	r.Password = password
 	r.Domain = domain
 	return r
+}
+
+func CreatePasswordFromJSON(json []byte) (*Password, error) {
+	username, err := jsonparser.GetString(json, "username")
+	if err != nil {
+		return nil, err
+	}
+	password, err := jsonparser.GetString(json, "password")
+	if err != nil {
+		return nil, err
+	}
+	domain, err := jsonparser.GetString(json, "domain")
+	if err != nil {
+		return nil, err
+	}
+	return CreatePassword(username, password, domain), nil
 }
 
 func (pw *Password) AddPolicies(policies ...Policy) {
